a1ab0bb0f0eea80887f6ce388f2bd398759389f9
sehoon95 <37127691+sehoon95@users.noreply.github.com>
2018-03-16 18:01:57 +0900

    project init
    init project

---
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,50 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+public class UserDao {
+    public User get(int id) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return user;
+    }
+
+    public Integer insert(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "insert into userinfo(name, password) values (?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Integer id = resultSet.getInt(1);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8"
+                , "root", "8877");
+    }
+}
